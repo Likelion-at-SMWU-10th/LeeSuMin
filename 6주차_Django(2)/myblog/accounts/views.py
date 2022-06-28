1f6bba59e3f1d4b71a13eb583b35da700ee6bb58
summit45 <brandy2145@gmail.com>
2022-06-28 18:52:04 +0900

    [feat] 8주차 세션 완료

--- 6주차_Django(2)/myblog/accounts/views.py
+++ 6주차_Django(2)/myblog/accounts/views.py
@@ -18,3 +18,25 @@ def signup(request):
             return render(request, 'accounts/signup.html', {'error' : '비밀번호가 일치하지 않습니다.'})
     else:
         return render(request, 'accounts/signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        id = request.POST['userId']
+        pwd = request.POST['password']
+        user = auth.authenticate(request, username=id, password=pwd)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else: #유저의 아이디, 비밀번호가 유효하지 않는 경우
+                return render(request, 'accounts/signin.html', {'error': '잘못된 아이디 또는 비밀번호 입니다.'})
+
+    else: # request.method == 'GET'
+        return render(request, 'accounts/signin.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+
